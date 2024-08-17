69def35d4be0f420e0496980d2061393ad6af875
ppabis <piotr@pabis.eu>
2024-08-17 21:30:03 +0200

    CloudFormation role to be able to control all resources

---
+++ role_cloudformation.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "CloudFormationTrustPolicy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudformation.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "CloudFromationRole" {
+  name               = "CloudFormationRole"
+  assume_role_policy = data.aws_iam_policy_document.CloudFormationTrustPolicy.json
+}
+
+resource "aws_iam_role_policy_attachment" "CloudFormationPolicyAttachment" {
+  # Quite a dumb way to define a role but meh, whatever ¯\_(ツ)_/¯
+  role       = aws_iam_role.CloudFromationRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
